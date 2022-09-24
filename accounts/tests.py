1375adc9a35075dee97327757b8066ee04ec8849
makan DIANKA <makan.dianka@hotmail.com>
2022-09-24 03:16:38 +0000

    unit test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from accounts.views import connexion
+
+class TestUrls(SimpleTestCase):
+    def test_url(self):
+        url = reverse('accounts:connexion')
+        self.assertEquals(resolve(url).func, connexion)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse("accounts:connexion")
+
+    def test_login_GET(self):
+        res=self.client.get(self.login_url)
+        self.assertEquals(res.status_code, 200)
+        self.assertTemplateUsed(res, 'accounts/login.html')
